221885284c6af51a429f29986d09fee3b59a8a22
DianaPVaz <dianapereiravaz@gmail.com>
2022-03-11 18:36:39 +0100

    icon update

--- src/index.js
+++ src/index.js
@@ -65,9 +65,8 @@ function displayWeatherCondition(response) {
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
-
   );
-let iconElement.setAttribute("alt",  response.data.weather[0].description);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function handleSubmit(event) {
   event.preventDefault();
