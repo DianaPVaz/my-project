0b0b394a967015bc6034d0eb38b6f74e6a810326
DianaPVaz <dianapereiravaz@gmail.com>
2022-03-11 17:50:50 +0100

    Description

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ function displayWeatherCondition(response) {
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather.description;
+    response.data.weather.[0].description;
 }
 function handleSubmit(event) {
   event.preventDefault();
