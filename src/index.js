fdbe2b041952650c023c91d66395ded2210de64d
DianaPVaz <dianapereiravaz@gmail.com>
2022-03-13 13:31:25 +0100

    Far add active

--- src/index.js
+++ src/index.js
@@ -92,6 +92,7 @@ function displayToFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
