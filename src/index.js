fcebc359d2b81d858ad89df641386f9adbe2a1da
DianaPVaz <dianapereiravaz@gmail.com>
2022-03-20 16:37:21 +0100

    Remove F conversion

--- src/index.js
+++ src/index.js
@@ -131,29 +131,29 @@ function getCurrentLocation(event) {
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-function displayToFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-let celsiusTemperature = null;
+///function displayToFahrenheitTemperature(event) {
+//event.preventDefault();
+//let temperatureElement = document.querySelector("#temperature");
+//celsiusLink.classList.remove("active");
+//fahrenheitLink.classList.add("active");
+//let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+//temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+//}
+//function displayCelsiusTemperature(event) {
+//event.preventDefault();
+//celsiusLink.classList.add("active");
+//fahrenheitLink.classList.remove("active");
+//let temperatureElement = document.querySelector("#temperature");
+//temperatureElement.innerHTML = Math.round(celsiusTemperature);
+//}
+//let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayToFahrenheitTemperature);
+//let fahrenheitLink = document.querySelector("#fahrenheit-link");
+//fahrenheitLink.addEventListener("click", displayToFahrenheitTemperature);
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
+//let celsiusLink = document.querySelector("#celsius-link");
+//celsiusLink.addEventListener("click", displayCelsiusTemperature);
 searchCity("New York");
